00070c21413183404295e170235966a11b45ad14
Santeri Edelmann <santeri.edelmann@gmail.com>
2023-10-08 13:23:16 +0300

    added a @license to metadata

--- jpdb-kanjivg-image-replacer.user.js
+++ jpdb-kanjivg-image-replacer.user.js
@@ -7,6 +7,7 @@
 // @match        https://jpdb.io/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=jpdb.io
 // @grant        GM_xmlhttpRequest
+// @license      CC BY 4.0
 // ==/UserScript==
 
 (function() {
